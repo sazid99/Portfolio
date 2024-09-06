74ce4b475da08bab8090fe5f4c112ca331bff44d
Mahmuduzzaman Sazid <mahmuduzzamansazid@gmail.com>
2024-09-06 15:17:10 +0600

    new commit add toogler button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import "./Navbar.css";
 
 function Navbar() {
-  const color = ["#686D76", "white" , "black"];
+  const color = ["#686D76", "white", "black"];
   const bodyTag = document.querySelector("body");
   var count = 0;
 
@@ -44,22 +44,74 @@ function Navbar() {
           <div id="detail-page">
             <h1 id="myName">Mahmuduzzaman Sazid</h1>
             <div id="right-info">
-              <a className="right-nav-info" href="#">
-                Service
-              </a>
-              <a className="right-nav-info" href="#">
-                Projects
-              </a>
-              <a className="right-nav-info" href="#">
-                Templates
-              </a>
-              <a className="right-nav-info" href="#">
-                Blog
-              </a>
-              <a className="right-nav-info" href="#">
-                Contact
-              </a>
-              <i className="fa-solid fa-bars"></i>
+              <div id="without-dropdown">
+                <a className="right-nav-info" href="#">
+                  Service
+                </a>
+                <a className="right-nav-info" href="#">
+                  Projects
+                </a>
+                <a className="right-nav-info" href="#">
+                  Templates
+                </a>
+                <a className="right-nav-info" href="#">
+                  Blog
+                </a>
+                <a className="right-nav-info" href="#">
+                  Contact
+                </a>
+              </div>
+              {/* nav bar toogler... */}
+              <nav id="nav-toogler-right" className="navbar fixed-top">
+                <div className="container-fluid">
+                  <p className="navbar-brand" href="#"></p>
+                  <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="offcanvas"
+                    data-bs-target="#offcanvasNavbar"
+                    aria-controls="offcanvasNavbar"
+                    aria-label="Toggle navigation"
+                  >
+                    <span className="navbar-toggler-icon"></span>
+                  </button>
+                  <div
+                    className="offcanvas offcanvas-end"
+                    id="offcanvasNavbar"
+                    aria-labelledby="offcanvasNavbarLabel"
+                  >
+                    <div className="offcanvas-header">
+                      <h5 className="offcanvas-title" id="offcanvasNavbarLabel">
+                        Offcanvas
+                      </h5>
+                      <button
+                        type="button"
+                        className="btn-close"
+                        data-bs-dismiss="offcanvas"
+                        aria-label="Close"
+                      ></button>
+                    </div>
+                    <div className="offcanvas-body">
+                      <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+                        <li className="nav-item">
+                          <a
+                            className="nav-link active"
+                            aria-current="page"
+                            href="#"
+                          >
+                            Home
+                          </a>
+                        </li>
+                        <li className="nav-item">
+                          <a className="nav-link" href="#">
+                            Link
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </nav>
             </div>
           </div>
         </div>
