a8729cad618deaf87ae98f00f3547b54c02a0453
Mahmuduzzaman Sazid <mahmuduzzamansazid@gmail.com>
2024-09-07 22:38:44 +0600

    new navbar style with bootstrap funtionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,121 +1,166 @@
 import "./Navbar.css";
 
 function Navbar() {
-  const color = ["#686D76", "white", "black"];
-  const bodyTag = document.querySelector("body");
-  var count = 0;
-
-  function changeColor() {
-    count++;
-    if (count >= color.length) {
-      count = 0;
-    }
-    bodyTag.style.background = color[count];
-    bodyTag.style.color = color[count] === "white" ? "black" : "white";
-  }
-
   return (
     <>
-      <div id="navbar-container">
-        <div id="navbar">
-          {/* Navcbar upper part */}
-          <div id="contact-page">
-            <div id="left-side-options">
-              <i id="phone" className="fa-solid fa-phone">
-                <a id="phone-a" href="#">
-                  {" "}
-                  +8801989399577
-                </a>
-              </i>
-              <i id="email" className="fa-solid fa-envelope">
-                <a id="email-a" href="#">
-                  mahmuduzzamansazid@gmail.com
-                </a>
-              </i>
-              <i
-                id="dark-light-mode"
-                className="fa-solid fa-circle-half-stroke"
-                onClick={changeColor}
-              ></i>
+      <nav className="navbar navbar-dark bg-dark navbar-expand-lg fixed-top">
+        <div className="container container-fluid">
+          <a className="navbar-brand" href="#">
+            <span id="nav-logo">
+              <i className="fa-brands fa-slack"></i>
+            </span>
+            <span id="nav-logo-s">S</span>
+            <span id="nav-logo-name">azid</span>
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasDarkNavbar"
+            aria-controls="offcanvasDarkNavbar"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="offcanvas offcanvas-end text-bg-dark"
+            id="offcanvasDarkNavbar"
+            aria-labelledby="offcanvasDarkNavbarLabel"
+          >
+            <div className="offcanvas-header">
+              <h5 className="offcanvas-title" id="offcanvasDarkNavbarLabel">
+                Menu
+              </h5>
+              <button
+                type="button"
+                className="btn-close btn-close-white"
+                data-bs-dismiss="offcanvas"
+                aria-label="Close"
+              ></button>
             </div>
-          </div>
+            <div className="offcanvas-body">
+              <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">
+                    Home
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Skills
+                  </a>
+                </li>
 
-          {/* Navcbar lower part */}
-          <div id="detail-page">
-            <h1 id="myName">Mahmuduzzaman Sazid</h1>
-            <div id="right-info">
-              <div id="without-dropdown">
-                <a className="right-nav-info" href="#">
-                  Service
-                </a>
-                <a className="right-nav-info" href="#">
-                  Projects
-                </a>
-                <a className="right-nav-info" href="#">
-                  Templates
-                </a>
-                <a className="right-nav-info" href="#">
-                  Blog
-                </a>
-                <a className="right-nav-info" href="#">
-                  Contact
-                </a>
-              </div>
-              {/* nav bar toogler... */}
-              <nav id="nav-toogler-right" className="navbar fixed-top">
-                <div className="container-fluid">
-                  <p className="navbar-brand" href="#"></p>
-                  <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="offcanvas"
-                    data-bs-target="#offcanvasNavbar"
-                    aria-controls="offcanvasNavbar"
-                    aria-label="Toggle navigation"
+                <li className="nav-item dropdown">
+                  <a
+                    className="nav-link dropdown-toggle"
+                    href="#"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
                   >
-                    <span className="navbar-toggler-icon"></span>
-                  </button>
-                  <div
-                    className="offcanvas offcanvas-end"
-                    id="offcanvasNavbar"
-                    aria-labelledby="offcanvasNavbarLabel"
+                    Services
+                  </a>
+                  <ul className="dropdown-menu dropdown-menu-dark">
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Web Development
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        App Development
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+                <li className="nav-item dropdown">
+                  <a
+                    className="nav-link dropdown-toggle"
+                    href="#"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
                   >
-                    <div className="offcanvas-header">
-                      <h5 className="offcanvas-title" id="offcanvasNavbarLabel">
-                        Offcanvas
-                      </h5>
-                      <button
-                        type="button"
-                        className="btn-close"
-                        data-bs-dismiss="offcanvas"
-                        aria-label="Close"
-                      ></button>
-                    </div>
-                    <div className="offcanvas-body">
-                      <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-                        <li className="nav-item">
-                          <a
-                            className="nav-link active"
-                            aria-current="page"
-                            href="#"
-                          >
-                            Home
-                          </a>
-                        </li>
-                        <li className="nav-item">
-                          <a className="nav-link" href="#">
-                            Link
-                          </a>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-              </nav>
+                    Projects
+                  </a>
+                  <ul className="dropdown-menu dropdown-menu-dark">
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Live Projects
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Themes
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Templates
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Blog
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Review
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Contact
+                  </a>
+                </li>
+              </ul>
+
+
+              <ul className="nav mt-5 d-lg-none">
+                <li className="nav-item">
+                  <a className="nav-link" aria-label="Whatsapp" href="#">
+                    <i className="fa-brands fa-whatsapp contact-icon whatsapp"></i>
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link " aria-label="Linkedin" href="#">
+                    <i className="fa-brands fa-linkedin contact-icon linkedin"></i>
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" aria-label="Facebook" href="#">
+                    <i className="fa-brands fa-facebook contact-icon facebook"></i>
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" aria-label="Twitter" href="#">
+                    <i className="fa-brands fa-twitter contact-icon twitter"></i>
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" aria-label="Instagram" href="#">
+                    <i className="fa-brands fa-instagram contact-icon instagram"></i>
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" aria-label="Youtube" href="#">
+                    <i className="fa-brands fa-youtube contact-icon youtube"></i>
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" aria-label="Github" href="#">
+                    <i className="fa-brands fa-github contact-icon github"></i>
+                  </a>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
-      </div>
+      </nav>
     </>
   );
 }
